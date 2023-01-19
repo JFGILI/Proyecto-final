0950271f73b3408317ca59e5319a7411ce8f3f31
Juan Francisco GILI <juanfrancisco54@hotmail.com>
2023-01-19 15:28:04 -0300

    imagenes productos

--- products/models.py
+++ products/models.py
@@ -4,9 +4,14 @@ class products (models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     stock = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products', verbose_name='Imagen')
     
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        
     
 class costumer (models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -14,7 +19,10 @@ class costumer (models.Model):
     phone_number= models.IntegerField()
     
     def __str__(self):
-        return self.phone_number
+        return self.name
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
 
 class Location (models.Model):
     name = models.CharField(max_length=20, )
@@ -22,3 +30,8 @@ class Location (models.Model):
     
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = 'Local'
+        verbose_name_plural = 'Locales'
+    
+   
--- resto/urls.py
+++ resto/urls.py
@@ -2,6 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import  settings
+from django.conf.urls.static import static
+
+
 from resto.views import index
 
 urlpatterns = [
@@ -10,6 +14,4 @@ urlpatterns = [
 
     
     path ('products/', include ('products.urls')),
-   
-
-]
+] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
