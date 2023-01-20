2037ad56015c07f8586ec670d9a323e522bba636
Juan Francisco GILI <juanfrancisco54@hotmail.com>
2023-01-20 16:06:02 -0300

    app users

--- resto/urls.py
+++ resto/urls.py
@@ -5,13 +5,13 @@ from django.urls import path, include
 from django.conf import  settings
 from django.conf.urls.static import static
 
-
 from resto.views import index
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
 
-    
     path ('products/', include ('products.urls')),
-] + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path ('users/', include ('users.urls')),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ users/views.py
@@ -0,0 +1,29 @@
+
+from django.shortcuts import render
+
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login,logout,authenticate
+
+def login_view(request):
+    if request.method == 'GET':
+        form = AuthenticationForm()
+        context ={
+            'form': form
+        }
+        return render(request, 'users/login.html', context=context)
+    
+    elif request.method == 'POST':
+        form = AuthenticationForm(request = request, data= request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get ('username')
+            password = form.cleaned_data.get ('password')
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                
+                context ={
+                    'message': f'Bienvenido {username} a Five Guys!'
+                }
+                return render (request, 'index.html', context = context)
+        
