ccc65c9cdf3cba5c8062e4df0e2bf9a4422f18b7
Juan Francisco GILI <juanfrancisco54@hotmail.com>
2023-01-20 21:16:05 -0300

    login, logout y singup

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth import login, authenticate
 
 def login_view(request):
     if request.method == 'GET':
@@ -21,9 +22,37 @@ def login_view(request):
             
             if user is not None:
                 login(request, user)
-                
+
                 context ={
                     'message': f'Bienvenido {username} a Five Guys!'
                 }
                 return render (request, 'index.html', context = context)
-        
+            
+        form = AuthenticationForm
+        context ={
+            'form': form,
+            'errors':'Usuario o contrasena incorrectos'
+        }
+        return render(request, 'users/login.html', context=context)
+
+def register(request):
+    if request.method =='GET':
+        form = UserCreationForm()
+        context ={
+            'form': form
+        }
+        return render(request, 'users/register.html', context=context)
+   
+    elif request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+            
+            context = {
+                'errors': form.errors,
+                'form': UserCreationForm()
+            }
+            return render(request, 'users/register.html', context=context)
+
+
